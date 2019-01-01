47465fac5a8969b21a3285149cbb525a6131d8ca
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2019-01-01 15:46:05 +0900

    working with SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp');
 var uglifyjs = require('uglify-js');
 var composer = require('gulp-uglify/composer');
@@ -8,18 +10,40 @@ var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sourceMaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
 
 // File paths
 var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
 var CSS_PATH = 'public/css/**/*.css';
 
-// Styles
+// // CSS Styles
+// gulp.task('styles', function () {
+//   console.log('Starting "styles" task.');
+
+//   // make sure reset.css comes first
+//   return gulp.src(['public/css/reset.css', CSS_PATH])
+//     .pipe(plumber(function (e) {
+//       console.log('Styles Task Error');
+//       console.log(e);
+//       this.emit('end');
+//     })) // error handling
+//     .pipe(sourceMaps.init())
+//     .pipe(autoprefixer()) // config ex. {browsers: ['last 2 versions', 'ie 8']}
+//     .pipe(concat('styles.css'))
+//     .pipe(minifyCss())
+//     .pipe(sourceMaps.write())
+//     .pipe(gulp.dest(DIST_PATH))
+//     .pipe(livereload());
+// });
+
+// SCSS Styles
 gulp.task('styles', function () {
   console.log('Starting "styles" task.');
 
-  // make sure reset.css comes first
-  return gulp.src(['public/css/reset.css', CSS_PATH])
+  return gulp.src('public/scss/styles.scss')
     .pipe(plumber(function (e) {
       console.log('Styles Task Error');
       console.log(e);
@@ -27,8 +51,7 @@ gulp.task('styles', function () {
     })) // error handling
     .pipe(sourceMaps.init())
     .pipe(autoprefixer()) // config ex. {browsers: ['last 2 versions', 'ie 8']}
-    .pipe(concat('styles.css'))
-    .pipe(minifyCss())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(sourceMaps.write())
     .pipe(gulp.dest(DIST_PATH))
     .pipe(livereload());
@@ -65,7 +88,8 @@ gulp.task('watch', function () {
   require('./server.js');
   livereload.listen();
   gulp.watch(SCRIPTS_PATH, ['scripts']);
-  gulp.watch(CSS_PATH, ['styles']);
+  // gulp.watch(CSS_PATH, ['styles']);
+  gulp.watch('public/scss/**/*.scss', ['styles']);
 });
 
 // Gulp 4 example - done is necessary to signal async completion
